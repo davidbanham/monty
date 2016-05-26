44eda6b830bc160b46dd820638ece2db19338b8d
David Banham <david@banham.id.au>
2016-05-26 16:49:19 +1000

    Binaries to subdir

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-GOOS=darwin GOARCH=amd64 go build -o monty-osx-arm64
-GOOS=linux GOARCH=arm64 go build -o monty-linux-arm64
-GOOS=linux GOARCH=amd64 go build -o monty-linux-amd64
-GOOS=linux GOARCH=386 go build -o monty-linux-386
-GOOS=windows GOARCH=386 go build -o monty-windows-386.exe
-GOOS=windows GOARCH=amd64 go build -o monty-windows-amd64.exe
+GOOS=darwin GOARCH=amd64 go build -o binaries/monty-osx-arm64
+GOOS=linux GOARCH=arm64 go build -o binaries/monty-linux-arm64
+GOOS=linux GOARCH=amd64 go build -o binaries/monty-linux-amd64
+GOOS=linux GOARCH=386 go build -o binaries/monty-linux-386
+GOOS=windows GOARCH=386 go build -o binaries/monty-windows-386.exe
+GOOS=windows GOARCH=amd64 go build -o binaries/monty-windows-amd64.exe
